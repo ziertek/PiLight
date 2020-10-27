cda6108a9ca854a20689b1c85f6c35e9caf82f4e
ziertek <BradenMagnan@live.ca>
2020-10-27 12:55:05 -0600

    Moved wrapper to lib Removed test file Updated server to see new wrapper location

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-from unicorn_wrapper import UnicornWrapper
+from lib.unicorn_wrapper import UnicornWrapper
 
 unicorn = UnicornWrapper()
 
 app = Flask(__name__)
 
-def setRGB(R,G,B):
-    unicorn.setAll(R,G,B)
-    return
+# def setRGB(R,G,B):
+#     unicorn.setAll(R,G,B)
+#     return
 
 @app.route("/")
 def controller():
@@ -16,37 +16,37 @@ def controller():
 # Colour routes
 @app.route('/api/Green')
 def Green():
-    setRGB(0,255,0)
+    #setRGB(0,255,0)
     print ("Green")
     return ("nothing")
 
 @app.route('/api/Red')
 def Red():
-    setRGB(255,0,0)
+    #setRGB(255,0,0)
     print ("Red")
     return ("nothing")
 
 @app.route('/api/Yellow')
 def Yellow():
-    setRGB(255,255,0)
+    #setRGB(255,255,0)
     print ("Yellow")
     return ("nothing")
 
 @app.route('/api/Blue')
 def Blue():
-    setRGB(0,0,255)
+    #setRGB(0,0,255)
     print ("Blue")
     return ("nothing")
 
 @app.route('/api/Pink')
 def Pink():
-    setRGB(255,0,255)
-    print ("Red")
+    #setRGB(255,0,255)
+    print ("Pink")
     return ("nothing")
 
 @app.route('/api/Teal')
 def Teal():
-    setRGB(0,255,255)
+    #setRGB(0,255,255)
     print ("Teal")
     return ("nothing")
 
